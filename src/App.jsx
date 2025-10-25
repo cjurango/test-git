683310d811c68c89c00a1d29675af248a3ae5695
Carlos Urango <cj.urango@gmail.com>
2025-10-25 10:37:09 -0500

    agregar estilos de h1

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Application de test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          The count is now {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
